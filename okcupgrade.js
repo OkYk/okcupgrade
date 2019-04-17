c91cb72a9ac3d05a7a987056677a5625a7450ee3
Sergey <OkYk@users.noreply.github.com>
2019-04-16 21:34:04 -0400

    OkFilterIt

--- okcupgrade.js
+++ okcupgrade.js
@@ -1,39 +1,81 @@
+// ==UserScript==
+// @name         OkFilterIt
+// @namespace
+// @version      0.1
+// @description  take over the world
+// @author       https://github.com/okyk
+// @match        https://www.okcupid.com/match
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
 var processed = processed || new Set();
-var bodytype = bodytype || new Set(["thin", "fit", "average", "average_build", "&mdash;"]);
-var nodiet = nodiet || new Set(["vegetarian", "vegan"]);
 var filtered = filtered || new Set();
+var pending = pending || new Set();
+
+const mustHave = new Array('straight', 'woman', 'single');
+const block = new Array("overweight", "full figured", "a little extra", "curvy", "queer", "trans", "transfeminine", "transgender", "transsexual", "nonconforming", "genderqueer", "pansexual", "demisexual", "questioning", "asexual", "heteroflexible", "gay", "lesbian", "bisexual", "homoflexible", "vegetarian", "vegan");
+
+const cardMatcher=".match-results-card";
+const cardProfileURL='.match-results-card';
+const profileFilterMatcher='.matchprofile-details-text';
 
 function filterit(match, url) {
+    if(url==null){
+        return;
+    }
 	jQuery.get(url).done(function(data) {
 		processed.add(url);
-		var pparams = jQuery.parseJSON(data.match(/ProfilePromo\.params[^\n]+/)[0].match(/{.*}/)[0]);
-		var profile = pparams.user.details.api_values;
-		if (profile.gender_tags.length != 0 
-			|| profile.ethnicity.indexOf("black") != -1 
-			|| profile.orientation != "straight" 
-			|| (profile.bodytype && !bodytype.has(profile.bodytype)) 
-			|| (profile.diet && nodiet.has(profile.diet))) {
+        pending.delete(url);
+		var plaintext = '';
+        var html=data.match(/var profileParams = [^\n]+/);
+        if(html==null){
+            console.log("UNR: "+url);
+            debugger;
+            return;
+        }
+		jQuery.parseJSON(data.match(/var profileParams = [^\n]+/)[0].match(/{.*}/)[0]).profile.details.forEach(function(element){plaintext+=' '+element.text.text.toLowerCase()});
+		block.forEach(function(element){
+			if (!match.hidden && plaintext.includes(element)){
 			match.hidden = true;
-			console.log("Filtered:" + url, profile);
+            match.remove();
+			console.log("Filtered ["+element+"]" + url+":>>"+plaintext);
 			filtered.add(url);
-		} else {
-			console.log(profile);
-		}
+			}
+		});
+		mustHave.forEach(function(element){
+			if (!match.hidden && !plaintext.includes(element)){
+			match.hidden = true;
+            match.remove();
+			console.log("Filtered:" + url+":>>"+plaintext);
+			filtered.add(url);
+			}
+		});
 	});
 }
+
 var filternonfit = function() {
 	var mills = 0;
-	jQuery('.match_card_wrapper.user-not-hidden.matchcard-user').each(function() {
-		var url = jQuery(jQuery(this).find('.image_link')[0]).attr('href');
+	jQuery(cardMatcher).each(function() {
+	var url = 'notfound';
+	if(cardMatcher!=cardProfileURL){
+		url = jQuery(jQuery(this).find(cardProfileURL)[0]).href;
+	} else {
+		url = this.href;
+	}
 		if (filtered.has(url)) {
 			this.hidden = true;
-		} else if (!processed.has(url)) {
+		} else if (!processed.has(url) && !pending.has(url)) {
+            pending.add(url);
 			setTimeout(filterit(this, url), mills);
-			mills += 300;
+			mills += 500;
 		} else {
-			console.log("Skipping");
+//			console.log("Skipping");
 		}
 
 	});
 };
-setInterval(filternonfit, 8000);
+setInterval(filternonfit, 300);
+})();
